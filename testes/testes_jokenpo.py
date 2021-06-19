feef000bb8bb85400ff625cb6516a450fced63ad
Guilherme Mattoso <guilhermemktfran@gmail.com>
2021-06-19 03:38:54 -0300

    atualizações

--- testes/testes_jokenpo.py
+++ testes/testes_jokenpo.py
@@ -1,3 +1,5 @@
+from time import sleep
+from random import randint
 print('''
                                                              JO KEN PO
 
@@ -18,19 +20,89 @@ regras='''- O torneio de Jo, Ken e Pô está preste a começar, mas antes vamos 
    -Jo, representa o poder Terra;
    -Ken, representa o poder Metal;
    -Po, representa o poder folha. 
-   3º Se divirta '''
+   3º Se divirta! '''
+pergunta=''
+rounds=0
+rodadas_ganhas=rodadas_peridas=empates=0
+print(regras)
 while True:
-   print(regras)
    rounds=int(input('Quantos rounds você quer jogar? '))
-   guerreiro=input('Qual guerreiro é você? ').upper()
-   guerreiro[0-1]
-   if guerreiro==guerreiro:
+   guerreiro=str(input('Qual guerreiro é você? ')).upper().strip()
+   
+   if guerreiro in "JO,KEN,PO":
        break
    else:
-     print('Inválido')
-     break
-print(guerreiro)
-print(rounds)
+      print('Inválido!Tente novamente!')
+      guerreiro=str(input('Qual guerreiro é você? ')).upper().strip()
+      continue
+if guerreiro in "JO,KEN,PO":
+     if guerreiro == "JO":
+          guerreiro=1
+     if guerreiro=="KEN":
+          guerreiro=2
+     if guerreiro=="PO":
+       guerreiro=3
+
+     for n in range(rounds):
+          npc=randint(1,3) 
+          print(f'\n{n+1}º rodada')                
+          if npc == 1 and guerreiro==2:
+               if guerreiro>npc:
+                    print('vc ganha')
+                    rodadas_ganhas+=1
+          if npc == 1 and guerreiro == 3:
+               if guerreiro>npc:
+                    print('vc perdeu')
+                    rodadas_peridas+=1
+          if npc == 2 and guerreiro == 1:
+               if npc>guerreiro:
+                    print('vc perdeu')
+                    rodadas_peridas+=1            
+          if npc == 2 and guerreiro == 3:
+               if npc<guerreiro:
+                    print('vc ganha') 
+                    rodadas_ganhas+=1   
+          if npc == 3 and guerreiro == 1:
+               if npc>guerreiro:
+                    print('vc ganha')
+                    rodadas_ganhas+=1
+          if npc == 3 and guerreiro == 2:
+               if npc>guerreiro:
+                    print('vc perdeu')
+                    rodadas_peridas+=1
+          if npc==guerreiro:
+               print('empate')
+               empates+=1
+          sleep(1)
+print(f'empates{empates}')
+print(f'ganhou {rodadas_ganhas}')
+print(f'perdeu {rodadas_peridas}')
+
+    
+               
+         
+         
+
+
+
+
+
+
+
+
+# if terra<metal:
+#      print('terra ganha de metal')
+# if terra<folha:
+#      print('folha ganha de terra')
+# if metal<folha:
+#      print('metal ganha de folha')
+# if metal>terra:
+#      print('metal perde para pedra')
+# if folha>terra:
+#      print('folha ganha de terra')
+# if folha>metal:
+#      print('folha perde para metal')
+
 
 
    
